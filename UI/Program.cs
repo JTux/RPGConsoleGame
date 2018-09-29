8414196692afff2216ba6c8c75f6ce5bc463cf5d
Joshua Tucker <thejltucker@gmail.com>
2018-09-28 23:01:18 -0400

    Initial

---
+++ UI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CharacterSuperModel characterSuperModel;
+            GameService gameService = new GameService();
+        }
+    }
+}
