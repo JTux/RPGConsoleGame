caf2464b036e4145955f699510d050c818d5ff8b
Justin Ahmann <jahmanndev@gmail.com>
2018-09-30 12:26:23 -0400

    cb lists added

--- Services/CombatService.cs
+++ Services/CombatService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace Services
 {
     public class CombatService
     {
+        private CharacterSuperModel _characterSuperModel;
+        private NPCS _nPCS;
+
+
+        public CharacterSuperModel Fight(CharacterSuperModel characterSuperModel, NPCS opponentList, int opponentID)
+        {
+            _characterSuperModel = characterSuperModel;
+            _nPCS = opponentList;
+
+
+
+            return _characterSuperModel;
+        }
     }
 }
